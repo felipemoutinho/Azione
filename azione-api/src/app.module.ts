3d6a2862a5d096995ee9ff0a91860ef0f4e76830
felipemoutinho <felipe.mg31@hotmail.com>
2021-05-23 11:44:05 -0300

    Configuração acesso ao banco postgres.

--- azione-api/src/app.module.ts
+++ azione-api/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 49155,
+      username: 'docker',
+      password: 'Aa123456',
+      database: 'postgres',
+      models: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- azione-api/src/app.service.ts
+++ azione-api/src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Sequelize } from 'sequelize-typescript';
 
 @Injectable()
 export class AppService {
+  
+  constructor(private sequelize: Sequelize) {
+  }
   getHello(): string {
+    this.sequelize
+        .authenticate()
+          .then(() => {
+            console.log('Connection has been established successfully.');
+          })
+          .catch(err => {
+            console.error('Unable to connect to the database:', err);
+          });
     return 'Hello World!';
   }
 }
